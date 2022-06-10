146544d38eae07a3fbbd55271b2fd3add462bf8e
Andrea Sarmiento <anbesamo@gmail.com>
2022-06-10 02:39:11 -0400

    adding some proto changes and master folder

---
+++ tp5-allende-duran-pedraza-sarmiento/master-folder/comands.sh
@@ -0,0 +1,9 @@
+
+dart pub global activate protoc_plugin
+
+export PATH="$PATH:$HOME/.pub-cache/bin"
+
+
+# protoc -I protos/ protos/todo.proto --dart_out=grpc:lib/src/generated
+
+protoc --dart_out=grpc:lib/src/generated -Iprotos protos/general.proto
---
+++ tp5-allende-duran-pedraza-sarmiento/master-folder/master.dart
@@ -0,0 +1,61 @@
+import 'package:grpc/grpc.dart';
+// import 'package:helloworld/src/generated/helloworld.pbgrpc.dart';
+import 'package:master-folder/generated/master-folder.pbgrpc.dart';
+
+
+//server.dart
+
+class GreeterService extends GreeterServiceBase {
+  @override
+  Future<HelloReply> sayHello(ServiceCall call, HelloRequest request) async {
+    return HelloReply()..message = 'Hello, ${request.name}!';
+  }
+}
+
+Future<void> main(List<String> args) async {
+  final server = Server(
+    [GreeterService()],
+    const <Interceptor>[],
+    CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
+  );
+  await server.serve(port: 50051);
+  print('Server listening on port ${server.port}...');
+}
+
+
+
+/// Dart implementation of the gRPC helloworld.Greeter client.
+
+
+
+Future<void> main(List<String> args) async {
+  final channel = ClientChannel(
+    'localhost',
+    port: 50051,
+    options: ChannelOptions(
+      credentials: ChannelCredentials.insecure(),
+      codecRegistry:
+          CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
+    ),
+  );
+  final stub = GreeterClient(channel);
+
+  final name = args.isNotEmpty ? args[0] : 'world';
+
+  try {
+    final response = await stub.sayHello(
+      HelloRequest()..name = name,
+      options: CallOptions(compression: const GzipCodec()),
+    );
+    print('Greeter client received: ${response.message}');
+  } catch (e) {
+    print('Caught error: $e');
+  }
+  await channel.shutdown();
+}
+
+
+
+// void main() {
+//     print('hello, world');
+// }
