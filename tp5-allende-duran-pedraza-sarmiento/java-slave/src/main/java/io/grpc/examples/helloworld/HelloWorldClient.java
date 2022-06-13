47655ff6b97a294fa9914dfe5e7b8086ddef9063
Stephany78315 <stephanyduran78315@gmail.com>
2022-06-13 14:43:43 -0400

    Added envarioment variables to java-slave

--- tp5-allende-duran-pedraza-sarmiento/java-slave/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ tp5-allende-duran-pedraza-sarmiento/java-slave/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -78,7 +78,11 @@ public class HelloWorldClient {
   public static void main(String[] args) throws Exception {
     String user = "Tefy-Slave-Java";
     // Access a service running on the local machine on port 50051
-    String target = "0.0.0.0:50500";
+    logger.log("Variable de entorno broker: {0}", System.getenv("SERVER"));
+    //logger.log("Variable de entorno puerto: {0}", System.getenv("PORT"));
+    logger.log("Conexion hecha a: {0}", System.getenv("SERVER"));
+    //String target = "0.0.0.0:50051";
+    String target = System.getenv("SERVER") + ":50051";
     // Allow passing in the user and target strings as command line arguments
     if (args.length > 0) {
       if ("--help".equals(args[0])) {
--- tp5-allende-duran-pedraza-sarmiento/java-slave/src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
+++ tp5-allende-duran-pedraza-sarmiento/java-slave/src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
@@ -33,7 +33,7 @@ public class HelloWorldServer {
 
   private void start() throws IOException {
     /* The port on which the server should run */
-    int port = 50500;
+    int port = 50051;
     server = ServerBuilder.forPort(port)
         .addService(new GreeterImpl())
         .build()
