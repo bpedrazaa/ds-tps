9ad15f37e3d9a0c878cfdd243a6f4e0c14389bd9
bpedrazaa <bpedrazaa@gmail.com>
2022-06-14 00:54:49 -0400

    Delete helloworld example

--- tp5-allende-duran-pedraza-sarmiento/ruby-slave/compile-proto.sh
+++ tp5-allende-duran-pedraza-sarmiento/ruby-slave/compile-proto.sh
@@ -3,8 +3,3 @@
 # Run this to compile the proto
 grpc_tools_ruby_protoc -I ../protos --ruby_out=lib --grpc_out=lib ../protos/general.proto
 echo ".proto file compiled"
-
-# helloworld.proto compilation
-grpc_tools_ruby_protoc -I . --ruby_out=lib --grpc_out=lib ./helloworld.proto
-echo "helloworld.proto file compiled"
-
