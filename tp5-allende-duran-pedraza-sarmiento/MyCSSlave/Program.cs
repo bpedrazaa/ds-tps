c40053b71cf4c0788ddb4b3b876e06ffd18a75eb
Andrea Sarmiento <anbesamo@gmail.com>
2022-06-16 14:19:48 -0400

    added c#

---
+++ tp5-allende-duran-pedraza-sarmiento/MyCSSlave/Program.cs
@@ -0,0 +1,36 @@
+using Grpc.Net.Client;
+using MyCSSlave.Services;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GenService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+string hostName = Dns.GetHostName();
+string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+Console.WriteLine(hostName);
+Console.WriteLine(myIP);
+Console.WriteLine("ENV VAR: "+Environment.GetEnvironmentVariable("SERVER"));
+
+var input = new RegistryInfo { IpAddress = myIP, Name = "CSharp-Slave" };
+
+string masterIP = Dns.GetHostByName(Environment.GetEnvironmentVariable("SERVER")).AddressList[0].ToString();
+string addr = "https://" +masterIP+ ":50051";
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+// var channel = GrpcChannel.ForAddress(ServerAddress, new GrpcChannelOptions { HttpHandler = httpHandler });
+Console.WriteLine("LINK CHANNEL: "+addr);
+var channel = GrpcChannel.ForAddress(addr, new GrpcChannelOptions { HttpHandler = httpHandler });
+var genClient = new GeneralService.GeneralServiceClient(channel);
+var reply = await genClient.RegisterToMasterAsync(input);
+app.Run();
---
+++ tp5-allende-duran-pedraza-sarmiento/MyCSSlave/Services/GenService.cs
@@ -0,0 +1,93 @@
+﻿using Grpc.Core;
+//using MyCSSlave.Protos;
+namespace MyCSSlave.Services
+{
+    public class GenService : GeneralService.GeneralServiceBase
+    {
+        private readonly ILogger<GenService> _logger;
+        private string sourceDirectory = "/proc";
+        public GenService(ILogger<GenService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<FileInfoList> GetFileInfo(Empty request, ServerCallContext context)
+        {
+            FileInfoList infoList = new FileInfoList();
+            int id = 0;
+            
+            try
+            {
+                Console.WriteLine( Environment.GetEnvironmentVariable("SERVER"));
+                var txtFiles = Directory.EnumerateFiles(this.sourceDirectory, "*.*");
+
+                foreach (string currentFile in txtFiles)
+                {
+                    id++;
+                    FileInfo myFile = new FileInfo();
+
+                    string fileName = currentFile.Substring(this.sourceDirectory.Length);
+                    myFile.FileName = fileName;
+                    long mynum = 0;
+                    myFile.Size = Convert.ToUInt32(mynum);
+                    System.IO.FileInfo fi = new System.IO.FileInfo(currentFile);
+                    if (fi.Exists)
+                    {
+                        myFile.Size = Convert.ToUInt32(fi.Length);
+                    }
+                    else 
+                    { 
+                        myFile.Size = Convert.ToUInt32(mynum);
+                    }
+                    myFile.SlaveId = System.Environment.MachineName;
+                    infoList.FileInfoList_.Add(myFile);
+                    Console.WriteLine(fileName);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return Task.FromResult(infoList);
+        }
+        public override Task<FileInfo> SearchFile(FileName request, ServerCallContext context)
+        {
+            string nombreBuscado = request.FileName_;
+            FileInfo myFile = new FileInfo();
+            int id = 0;
+            
+            try
+            {
+                var txtFiles = Directory.EnumerateFiles(this.sourceDirectory, "*.*");
+
+                foreach (string currentFile in txtFiles)
+                {
+                    id++;
+
+
+                    string fileName = currentFile.Substring(this.sourceDirectory.Length);
+
+                    System.IO.FileInfo fi = new System.IO.FileInfo(currentFile);
+                    if (fi.Exists)
+                    {
+                        if (fileName == nombreBuscado)
+                        {
+                            myFile.FileName = fileName;
+                            myFile.Size = Convert.ToUInt32(fi.Length);
+                            myFile.SlaveId = System.Environment.MachineName;
+                            Console.WriteLine(fileName);
+                            return Task.FromResult(myFile);
+                        }
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return Task.FromResult(myFile);
+        }
+    }
+}
